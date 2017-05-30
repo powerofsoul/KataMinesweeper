7fcba670e7c3b43583c6b21efc55c6264b0274fa
Florin Munteanu <florin.munteanu@napa.fi>
2017-05-30 14:36:00 +0300

    Test if map argument is valid
    Signed-off-by: Florin Munteanu <florin.munteanu@napa.fi>

---
+++ Minesweeper/MinesweeperTests/MinesweeperTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+usi
+namespace MinesweeperTests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        [ExpectedException (typeof(ArgumentException),"Invalid input")]
+        public void SizeOfGridIsIncorrect() {
+            string input = "***";
+            Map m = new Map(input);
+        }
+    }
+}
