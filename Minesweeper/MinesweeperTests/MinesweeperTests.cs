9f5290ca01d7814467c27090a821c4487cdc8116
Florin Munteanu <florin.munteanu@napa.fi>
2017-05-30 14:52:41 +0300

    Add test to check if input has invalid chars
    Signed-off-by: Florin Munteanu <florin.munteanu@napa.fi>

--- Minesweeper/MinesweeperTests/MinesweeperTests.cs
+++ Minesweeper/MinesweeperTests/MinesweeperTests.cs
@@ -12,5 +12,13 @@ namespace MinesweeperTests {
             int size = 10;
             Map m = new Map(input,10);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),"Invalid input")]
+        public void InvalidCharactersInInput() {
+            string input = "*.*.*#...";
+            int size = 3;
+            Map m = new Map(input,3);
+        }
     }
 }
