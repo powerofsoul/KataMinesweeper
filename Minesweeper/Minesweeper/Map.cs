193578c554eee86988b465759b30b37a6379cfce
Florin Munteanu <florin.munteanu@napa.fi>
2017-05-30 14:56:06 +0300

    Check if input map has valid chars
    Signed-off-by: Florin Munteanu <florin.munteanu@napa.fi>

--- Minesweeper/Minesweeper/Map.cs
+++ Minesweeper/Minesweeper/Map.cs
@@ -5,9 +5,18 @@ namespace Minesweeper {
     public class Map {
 
         public Map(string map,int size) {
-            if(map.Length != size * size) {
+            if(map.Length != size * size || !ValidMap(map)) {
                 throw new ArgumentException("Invalid input");
             }
+            
+        }
+
+        private bool ValidMap(string map) {
+            foreach(char c in map) {
+                if(c != '.' && c != '*')
+                    return false;
+            }
+            return true;
         }
     }
 }
