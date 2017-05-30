e9c5f42d499a2063918b5e7698fc3d12b0c72cfd
Florin Munteanu <florin.munteanu@napa.fi>
2017-05-30 17:01:48 +0300

    Add categories to tests
    Signed-off-by: Florin Munteanu <florin.munteanu@napa.fi>

--- Minesweeper/MinesweeperTests/MinesweeperTests.cs
+++ Minesweeper/MinesweeperTests/MinesweeperTests.cs
@@ -7,6 +7,7 @@ namespace MinesweeperTests {
     [TestClass]
     public class UnitTest1 {
 
+        [TestCategory("MapCreation")]
         [TestMethod]
         [ExpectedException(typeof(ArgumentException),"Invalid size")]
         public void SizeOfGridIsIncorrect() {
@@ -14,6 +15,7 @@ namespace MinesweeperTests {
             Map m = new Map(input,5,5);
         }
 
+        [TestCategory("MapCreation")]
         [TestMethod]
         [ExpectedException(typeof(ArgumentException),"Invalid map")]
         public void InvalidCharactersInInput() {
@@ -21,41 +23,48 @@ namespace MinesweeperTests {
             Map m = new Map(input,3,3);
         }
 
+        [TestCategory("MapCreation")]
         [TestMethod]
         public void MapIsValid() {
             string input = ".*..*.***";
             Map m = new Map(input,3,3);
         }
 
+        [TestCategory("LoadFromFile")]
         [TestMethod]
         public void MapFromFileIsValid() {
             var m = Map.LoadMapFromFile(@"Resources\map1.txt");
         }
 
+        [TestCategory("LoadFromFile")]
         [TestMethod]
         [ExpectedException(typeof(ArgumentException),"Invalid size")]
         public void MapFromFileHasInvalidSize() {
             var m = Map.LoadMapFromFile(@"Resources\map2.txt");
         }
 
+        [TestCategory("LoadFromFile")]
         [TestMethod]
         [ExpectedException(typeof(ArgumentException),"Invalid map")]
         public void MapFromFileHasInvalidCharacters() {
             var m = Map.LoadMapFromFile(@"Resources\map3.txt");
         }
 
+        [TestCategory("LoadFromFile")]
         [TestMethod]
         public void FilesHasTwoMaps() {
             var maps = Map.LoadMapFromFile(@"Resources\map4.txt");
             Assert.AreEqual(maps.Count,2);
         }
 
+        [TestCategory("LoadFromFile")]
         [TestMethod]
         public void ZeroMapIsSkipedFromLoadMapFromFile() {
             var maps = Map.LoadMapFromFile(@"Resources\map5.txt");
             Assert.AreEqual(maps.Count,1);
         }
 
+        [TestCategory("MapCreation")]
         [TestMethod]
         public void ParseMapReturnNoExceptions() {
             Map.ParseMap(new string[] { "2 2","..",".." });
