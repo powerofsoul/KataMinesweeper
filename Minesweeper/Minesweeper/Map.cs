8d0dd23496b2cbf26a413cc72b88e90b5a8b0855
Florin Munteanu <florinmunteanu96@gmail.com>
2017-06-03 10:50:29 +0300

    Add GetMatrix method and fix GetField
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

--- Minesweeper/Minesweeper/Map.cs
+++ Minesweeper/Minesweeper/Map.cs
@@ -3,55 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace Minesweeper {
+namespace Minesweeper
+{
 
-    public class Map {
+    public class Map
+    {
         public int Width;
         public int Height;
         public string MapLayout;
 
-        public Map(string mapLayout,int width,int height) {
+        public Map(string mapLayout,int width,int height)
+        {
             ValidMap(mapLayout,width,height);
             Width = width;
             Height = height;
             MapLayout = mapLayout;
         }
 
-        private bool ValidMap(string map,int width,int height) {
+        private bool ValidMap(string map,int width,int height)
+        {
             if(width * height != map.Length)
                 throw new ArgumentException("Invalid size");
-            foreach(char c in map) {
+            foreach(char c in map)
+            {
                 if(c != '.' && c != '*')
                     throw new ArgumentException("Invalid map");
             }
             return true;
         }
 
-        public static List<Map> LoadMapFromFile(string path) {
+        public static List<Map> LoadMapFromFile(string path)
+        {
             var fileContent = System.IO.File.ReadLines(path).ToArray();
             List<Map> maps = new List<Map>();
             int i = 0;
-            while(i < fileContent.Length) {
-                if(string.IsNullOrWhiteSpace(fileContent[i])) {
+            while(i < fileContent.Length)
+            {
+                if(string.IsNullOrWhiteSpace(fileContent[i]))
+                {
                     i++;
                     continue;
                 }
                 Regex r = new Regex(@"(\d+)\s+");
                 var matches = r.Match(fileContent[i]);
-                
+
                 var height = Convert.ToInt32(matches.Groups[1].Value);
-                if(height == 0) {
+                if(height == 0)
+                {
                     i++;
                     continue;
                 }
-                maps.Add(ParseMap(fileContent.Skip(i).Take(height+1).ToArray()));
+                maps.Add(ParseMap(fileContent.Skip(i).Take(height + 1).ToArray()));
                 i = i + height + 1;
             }
             return maps;
 
         }
 
-        public static Map ParseMap(string[] lines) {
+        public static Map ParseMap(string[] lines)
+        {
             Regex r = new Regex(@"(\d+)\s+(\d+)");
             var matches = r.Match(lines[0]);
 
@@ -63,13 +73,13 @@ namespace Minesweeper {
             return new Map(string.Join("",mapLayout),width,height);
         }
 
-        public char GetField(int x, int y)
+        public char GetField(int x,int y)
         {
             CheckInvalidPosition(x,y);
-            return MapLayout[y * Width + x];
+            return MapLayout[x * Width + y];
         }
 
-        public void SetField(char c, int x,int y)
+        public void SetField(char c,int x,int y)
         {
             CheckInvalidPosition(x,y);
             MapLayout = MapLayout.ReplaceCharAt(c,y * Width + x);
@@ -81,5 +91,17 @@ namespace Minesweeper {
                 throw new ArgumentException("Invalid position");
         }
 
+        public char[,] GetMatrix()
+        {
+            var matrix = new char[Width,Height];
+
+            for(int i = 0;i < Width;i++)
+                for(int j = 0;j < Height;j++)
+                {
+                    matrix[i,j] = GetField(i,j);
+                }
+
+            return matrix;
+        }
     }
 }
